1a9e068b6032e769aef4ab3e8e4cee454fa5cd8f
Rafael Nunes G. da Silveira <rngs@cin.ufpe.br>
2015-10-24 23:39:07 -0300

    Implementação AhoCorasick

---
+++ src/search/AhoCorasick.h
@@ -0,0 +1,27 @@
+//
+//  aho-corasick.hpp
+//  PCC
+//
+//  Created by Rafael Nunes G. da Silveira on 23/10/2015.
+//  Copyright © 2015 Rafael Nunes. All rights reserved.
+//
+
+#ifndef aho_corasick_hpp
+#define aho_corasick_hpp
+
+#include <vector>
+#include <unordered_map>
+#include "OccurrenceList.hpp"
+
+// Considera-se o alfabeto o conjunto de valores da tabela ASCII
+class Node {
+    
+public:
+    Node *fail;
+    OccurrenceList ocurrencies;
+    std::unordered_map<char , Node*> transitions;
+    
+    Node();
+};
+
+#endif /* aho_corasick_hpp */
---
+++ src/search/OccurrenceList.h
@@ -0,0 +1,31 @@
+//
+//  OccurrenceList.hpp
+//  PCC
+//
+//  Created by Rafael Nunes G. da Silveira on 24/10/2015.
+//  Copyright © 2015 Rafael Nunes. All rights reserved.
+//
+
+#ifndef OccurrenceList_hpp
+#define OccurrenceList_hpp
+
+#include <string>
+
+class OccurrenceListNode {
+public:
+    std::string occurrence;
+    OccurrenceListNode* next;
+    OccurrenceListNode(std::string occurrence);
+};
+
+class OccurrenceList {
+public:
+    OccurrenceListNode *head;
+    OccurrenceListNode *tail;
+    
+    OccurrenceList();
+    void push_front(std::string occurrence);
+    void push_back(OccurrenceList *list);
+};
+
+#endif /* OccurrenceList_hpp */
